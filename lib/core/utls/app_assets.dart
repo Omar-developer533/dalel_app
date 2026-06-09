d887eb485995bfd13c91213fdd66e1db5fd3d706
Omar <abdkaheel9@gmail.com>
2026-06-09 17:44:12 +0300

    add images to assets

---
+++ lib/core/utls/app_assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesForget
+  /// assets/images/forget.png
+  static const String imagesForget = "assets/images/forget.png";
+
+  /// Assets for imagesOmBoarding3
+  /// assets/images/om_boarding3.png
+  static const String imagesOmBoarding3 = "assets/images/om_boarding3.png";
+
+  /// Assets for imagesOnBoarding
+  /// assets/images/on_boarding.png
+  static const String imagesOnBoarding = "assets/images/on_boarding.png";
+
+  /// Assets for imagesOnBoarding2
+  /// assets/images/on_boarding2.png
+  static const String imagesOnBoarding2 = "assets/images/on_boarding2.png";
+
+  /// Assets for imagesSuccessicon
+  /// assets/images/successicon.png
+  static const String imagesSuccessicon = "assets/images/successicon.png";
+
+  /// Assets for imagesVector1
+  /// assets/images/Vector1.svg
+  static const String imagesVector1 = "assets/images/Vector1.svg";
+
+  /// Assets for imagesVector2
+  /// assets/images/Vector2.svg
+  static const String imagesVector2 = "assets/images/Vector2.svg";
+}
+
