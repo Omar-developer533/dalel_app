c0e9fc86252c79ec3078324a3e1574ab795e74ff
Omar <abdkaheel9@gmail.com>
2026-06-11 13:17:21 +0300

    app Text style

---
+++ lib/core/utls/app_styles.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class AppStyles {
+  static const stylesRegular64 = TextStyle(
+    fontFamily: 'Pacifico',
+    fontSize: 64,
+    fontWeight: FontWeight.w400,
+  );
+  static const stylesMedium24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+  );
+  static const styleLight16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w300,
+  );
+  static const stylesRegular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+  static const stylesMedium18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+  static const stylesSemiBold28 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w600,
+  );
+  static const stylesMedium12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+  static const stylesRegular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+  static const stylesBold32 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+  );
+  static const stylesSemiBold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+  static const stylesSemiBold12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+  );
+  static const stylesRegular22 = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+  );
+  static const stylesRegular20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+  );
+  static const stylesRegular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );  static const stylesBold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );
+}
