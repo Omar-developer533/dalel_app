a733e8acc904e89f93449cb0635a6a363366b798
Omar <abdkaheel9@gmail.com>
2026-06-11 17:09:55 +0300

    splash View

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,6 +1,11 @@
+import 'package:dalel/featurs/onboarding/presentation/views/on_boarding_one.dart';
 import 'package:dalel/featurs/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
+const kOnBoardingOne = '/OnBoardingOne';
 final GoRouter router = GoRouter(
-  routes: [GoRoute(path: '/', builder: (context, state) => SplashView())],
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => SplashView()),
+    GoRoute(path: kOnBoardingOne, builder: (context, state) => OnBoardingOne()),
+  ],
 );
--- lib/featurs/splash/presentation/views/splash_view.dart
+++ lib/featurs/splash/presentation/views/splash_view.dart
@@ -1,10 +1,45 @@
+import 'package:dalel/core/router/app_router.dart';
+import 'package:dalel/core/utls/app_color.dart';
+import 'package:dalel/core/utls/app_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 5), () {
+      context.go(kOnBoardingOne);
+    });
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColor.offWhite,
+
+      body: TweenAnimationBuilder(
+        tween: AlignmentGeometryTween(
+          begin: Alignment.topCenter,
+          end: Alignment.center,
+        ),
+        duration: Duration(seconds: 2),
+        builder: (context, value, child) => Align(
+          alignment: value!,
+          child: Text(
+            'Dalel',
+            style: AppStyles.stylesRegular64.copyWith(
+              color: AppColor.primaryColor,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
