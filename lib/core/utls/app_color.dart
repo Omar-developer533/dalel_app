a735c926b13f6518a1a660a1df429252302c8c65
Omar <abdkaheel9@gmail.com>
2026-06-11 12:07:21 +0300

    app colors

---
+++ lib/core/utls/app_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const primaryColor = Color(0xffC49E85);
+  static const offWhite = Color(0xffF8F4F9);
+  static const deepBrown = Color(0xff6B4B3E);
+  static const deepGray = Color(0xff6F6460);
+  static const lightGray = Color(0xffB4B4B4);
+    static const black = Color(0xff000000);
+
+}
